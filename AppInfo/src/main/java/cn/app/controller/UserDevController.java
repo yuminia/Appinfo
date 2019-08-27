96d0fc518be9cc03dcc3421b3e0412d0feb28ba7
liuzhi <287595442@qq.com>
2019-08-27 17:57:59 +0800

    登录

--- AppInfo/src/main/java/cn/app/controller/UserDevController.java
+++ AppInfo/src/main/java/cn/app/controller/UserDevController.java
@@ -2,6 +2,8 @@ package cn.app.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,55 @@ import cn.app.bean.UserDev;
 import cn.app.service.UserDevService;
 
 @Controller
-@RequestMapping("userDev")
+@RequestMapping("/app/userDev")
 public class UserDevController {
 	@Autowired
 	private UserDevService userDevService;
+	
 	
 	@RequestMapping("userDevList")
 	@ResponseBody
 	public List<UserDev> userDevList(){
 		return userDevService.getUserDevList(null);
+	}
+	
+	/** 跳转 Dev 用户登录页面 */
+	@RequestMapping("userDevLogin")
+	public String userDevLogin(){
+		return "userDev/userDevLogin";
+	}
+	
+	/** Dev 用户登录 提交 */
+	@RequestMapping("userDevLoginSubmit")
+	public String userDevLoginSubmit(UserDev userDev,HttpServletRequest request){
+		UserDev dbuserDev = userDevService.getUserDevByUserDevCodeAndPassword(userDev);
+		if(dbuserDev != null){
+			request.getSession().setAttribute("loginUserDev", dbuserDev);
+			return "userDev/main";
+		}else{
+			return "userDev/userDevLogin";
+		}
+	}
+	
+	/** Dev 用户注册 提交 */
+	@RequestMapping("userDevRegisterSubmit")
+	public String userDevRegisterSubmit(UserDev userDev){
+		int res = userDevService.addUserDev(userDev);
+		if(res == 1){
+			return "userDev/userDevLogin";
+		}
+		return null;
+	}
+	
+	/** ajax匹配 DevCode */
+	@ResponseBody
+	@RequestMapping("checkDevCode")
+	public String checkDevCode(String devCode){
+		UserDev userDev = userDevService.getUserDevByUserDevCode(devCode);
+		if(userDev != null){
+			return "true";
+		}else{
+			return "false";
+		}
 	}
 }
